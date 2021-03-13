97e7b658eb667dd87218b1a1da2a44ed9d7203b3
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-03-13 12:37:46 +0300

    Update keywords.py

--- streamlit-tags/keywords.py
+++ streamlit-tags/keywords.py
@@ -1,5 +1,4 @@
 import os
-from typing import Tuple
 
 import streamlit.components.v1 as components
 
@@ -45,42 +44,8 @@ else:
 # `declare_component` and call it done. The wrapper allows us to customize
 # our component's API: we can pre-process its input args, post-process its
 # output value, and add a docstring for users.
-def my_component(name, key=None):
-    """Create a new instance of "my_component".
-
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
-
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
-    """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _component_func(name=name, key=key, default=0)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
-
-
-def st_keywords(label: str, text: str, value: str, key=None) -> int:
-    component_value = _component_func(label=label, text=text, initialValue=[value], key=key, default=[value])
+def st_keywords(label: str, text: str, value: list, key=None) -> int:
+    component_value = _component_func(label=label, text=text, initialValue=value, key=key, default=value)
     return component_value
 
 
@@ -100,6 +65,6 @@ if not _RELEASE:
     # and lose its current state. In this case, we want to vary the component's
     # "name" argument without having it get recreated.
 
-    v_custom = st_keywords('Hello world', 'deep', 'one')
-    st.sidebar.write(v_custom)
+    v_custom = st_keywords('Hello world', 'deep', ['ione','2'])
+    st.write(v_custom)
 
