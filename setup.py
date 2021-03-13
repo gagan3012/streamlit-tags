0cd5d43b0bb1878b90f34a653504ec3c1350a2a2
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-03-14 00:13:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
+from os.path import dirname
+from os.path import join
 import setuptools
 
+
+def readme() -> str:
+    """Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top
+    level README file and 2) it's easier to type in the README file than to put
+    a raw string in below.
+    :return: content of README.md
+    """
+    return open(join(dirname(__file__), "README.md")).read()
+
+
 setuptools.setup(
-    name="streamlit-custom-component",
-    version="0.0.1",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
-    long_description_content_type="text/plain",
-    url="",
+    name="streamlit-tags",
+    version="0.1.0",
+    author="Gagan Bhatia",
+    author_email="gbhatia880@gmail.com",
+    description="Tags custom component for Streamlit",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gagan3012/streamlit-tags",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
     install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
         "streamlit >= 0.63",
-    ],
+    ]
 )
