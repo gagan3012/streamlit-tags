9956d90aa0e3db144bd0b96ef100c0bbd08a3f35
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-03-16 18:20:42 +0300

    Update app_sidebar.py

--- examples/app_sidebar.py
+++ examples/app_sidebar.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_tags import st_tags_sidebar
 
 
-keywords = st_tags_sidebar('Enter Keywords:', 'Press enter to add more', ['Zero', 'One', 'Two'])
+keywords = st_tags_sidebar('Enter Keywords:', 'Press enter', ['Zero', 'One', 'Two'])
 
 st.write("### Results:")
 st.write(keywords)
