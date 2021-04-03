64e44a19c5a7efe1e10386079702f89c63cb4d44
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-03 16:59:15 +0300

    Updates to setup.py [#5 ]

--- examples/app.py
+++ examples/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_tags import st_tags
 
 
-keywords = st_tags('# Enter Keywords:', 'Press enter to add more', ['Zero', 'One', 'Two'])
+keywords = st_tags('Enter Keywords:', 'Press enter to add more', ['Zero', 'One', 'Two'])
 
 st.write("### Results:")
 st.write(keywords)
