6194ce4c05c82bb8d37e6ab13f3a7d27e0316136
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-08 00:46:35 +0300

    Delete app_sidebar.py

--- examples/app_sidebar.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from streamlit_tags import st_tags_sidebar
-
-
-keywords = st_tags_sidebar('Enter Keywords:', 'Press enter', ['Zero', 'One', 'Two'])
-
-st.write("### Results:")
-st.write(keywords)
-
