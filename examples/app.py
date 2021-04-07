786dd5a4edcc13e3bc60dcb8a7909998acc916bb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-07 14:05:48 +0300

    Update app.py

--- examples/app.py
+++ examples/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_tags import st_tags
 
 with st.echo():
+    st.write("# Code for streamlit tags")
+
     keywords = st_tags(label='# Enter Keywords:',
                        text='Press enter to add more',
                        value=['Zero', 'One', 'Two'],
